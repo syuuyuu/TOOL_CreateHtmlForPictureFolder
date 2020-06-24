d3fb617414f41408cad31b1259a3aba10917dea2
syuuZhou <35445612+syuuyuu@users.noreply.github.com>
2020-06-24 14:23:46 +0900

    can set width height colunm color from args now

--- CreateHTMLpicFolder/FolderWithFileAndFolder.cs
+++ CreateHTMLpicFolder/FolderWithFileAndFolder.cs
@@ -1,92 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Collections;
-using System.IO;
 
 namespace CreateHTMLpicFolder
 {
-    class FolderWithFileAndFolder
+    internal class FolderWithFileAndFolder
     {
-        string MyFolderName = "FOLDER_NAME";
-        string MyFolderPath = "FOLDER_PATH";
-        int MyFolderLevel = 0;
-        string LEVEL_MARK = "** ";
-        string STEP_MARK = "-- ";
+        public static int ParaMaxColumn = 4;
+        public static int ParaPicHeight = 364;
+        public static int ParaPicWidth = 384;
+        public static string ParaBackgroundColor = "000000";
+        private const string STR_HTML = ".html";
+        private const string STR_INDEX = "index";
+        private const string STR_LEVEL_MARK = "** ";
+        private const string STR_STEP_MARK = "-- ";
+        private List<string> ContentList = new List<string>();
+        private List<string> FileList = new List<string>();
+        private List<FolderWithFileAndFolder> FolderList = new List<FolderWithFileAndFolder>();
+        private int MyFolderLevel = 0;
+        private string MyFolderName = "FOLDER_NAME";
+        private string MyFolderPath = "FOLDER_PATH";
+        private string ParentFolderName = null;
+        private int StepLevel = 0;
 
-        private void WriteToConsole(int step, string str)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int counti = 0; counti < MyFolderLevel; counti++)
-            {
-                sb.Append(LEVEL_MARK);
-            }
-            for (int counti = 0; counti < step; counti++)
-            {
-                sb.Append(STEP_MARK);
-            }
-
-            Console.WriteLine(sb.ToString() + str);
-        }
-
-
-        public FolderWithFileAndFolder SetFolderName(string name)
-        {
-            this.MyFolderName = name;
-            return this;
-        }
-        public FolderWithFileAndFolder SetFolderPath(string path)
-        {
-            this.MyFolderPath = path;
-            this.MyFolderName = Path.GetFileName(path);
-            return this;
-        }
-
-        List<FolderWithFileAndFolder> FolderList = new List<FolderWithFileAndFolder>();
-        List<string> FileList = new List<string>();
-        List<string> ContentList = new List<string>();
-
-        public static FolderWithFileAndFolder GetNewFolder()
+        public static FolderWithFileAndFolder GetNewInstance()
         {
             return new FolderWithFileAndFolder();
         }
 
-        public string GetFolderName()
-        {
-            return MyFolderName;
-        }
-        public string GetFolderPath()
-        {
-            return MyFolderPath;
-        }
-
-        public void GetFoldersUnder()
-        {
-            FolderList.Clear();
-            var folder_name_list = Directory.GetDirectories(MyFolderPath).ToList<string>();
-            foreach (string folder_name in folder_name_list)
-            {
-                var folder = FolderWithFileAndFolder.GetNewFolder().SetFolderPath(folder_name);
-                FolderList.Add(folder);
-                WriteToConsole(4, "New folder: " + folder.GetFolderPath());
-            }
-        }
-
-
-        public void GetFilesUnder()
-        {
-            FileList.Clear();
-            List<String> file_path_list = Directory.GetFiles(MyFolderPath).ToList<string>();
-            foreach (string file_path in file_path_list)
-            {
-                var file_name = Path.GetFileName(file_path);
-                FileList.Add(file_name);
-                WriteToConsole(1, "New file: " + file_name);
-            }
-        }
         public void GetContentsFromFileList()
         {
+            StepLevel++;
             List<string> jpg_list;
             jpg_list = ExtractJPGList();
             foreach (string jpg in jpg_list)
@@ -94,52 +40,103 @@ namespace CreateHTMLpicFolder
                 if (FileList.Contains(jpg))
                 {
                     ContentList.Add(jpg);
-                    WriteToConsole(3, "New Content: " + jpg);
+                    ShowToConsole(StepLevel, "New Content: " + jpg);
                 }
             }
         }
 
-
-        const string HTML = ".html";
-
-        string ParentFolderName = null;
-        const string INDEX = "index";
-        public void SearchAllContentUnder(int level, string parent_name)
+        public string GetFolderName()
         {
+            return MyFolderName;
+        }
+
+        public string GetFolderPath()
+        {
+            return MyFolderPath;
+        }
+
+        public void SearchAll(int level, string parent_name)
+        {
+            StepLevel = 0;
             if (level == 1)
             {
-                ParentFolderName = INDEX;
+                ParentFolderName = STR_INDEX;
             }
             else
             {
                 ParentFolderName = parent_name;
             }
             this.MyFolderLevel = level;
-            GetAllContentSelf();
-            GetFoldersUnder();
+            SearchContentSelf();
+            SearchFoldersUnder();
             foreach (FolderWithFileAndFolder folder in FolderList)
             {
                 try
                 {
-                    folder.SearchAllContentUnder(this.MyFolderLevel + 1, MyFolderName);
-                } catch (Exception ex)
+                    folder.SearchAll(this.MyFolderLevel + 1, MyFolderName);
+                }
+                catch (Exception ex)
                 {
-                    WriteToConsole(0, ex.ToString());
+                    ShowToConsole(StepLevel, ex.ToString());
                 }
             }
             String root_name = null;
             if (MyFolderLevel == 0)
             {
-                root_name = INDEX;
+                root_name = STR_INDEX;
             }
             WriteFoldersLinkToHTML(root_name);
             WriteContentsToHTML(root_name);
         }
 
+        public void SearchContentSelf()
+        {
+            SearchFilesUnder();
+            GetContentsFromFileList();
+        }
+        public void SearchFilesUnder()
+        {
+            StepLevel++;
+            FileList.Clear();
+            List<String> file_path_list = Directory.GetFiles(MyFolderPath).ToList<string>();
+            foreach (string file_path in file_path_list)
+            {
+                var file_name = Path.GetFileName(file_path);
+                FileList.Add(file_name);
+                ShowToConsole(StepLevel, "New file: " + file_name);
+            }
+        }
+
+        public void SearchFoldersUnder()
+        {
+            StepLevel++;
+            FolderList.Clear();
+            var folder_name_list = Directory.GetDirectories(MyFolderPath).ToList<string>();
+            foreach (string folder_name in folder_name_list)
+            {
+                var folder = FolderWithFileAndFolder.GetNewInstance().SetFolderPath(folder_name);
+                FolderList.Add(folder);
+                ShowToConsole(StepLevel, "New folder: " + folder.GetFolderPath());
+            }
+        }
+
+        public FolderWithFileAndFolder SetFolderName(string name)
+        {
+            this.MyFolderName = name;
+            return this;
+        }
+
+        public FolderWithFileAndFolder SetFolderPath(string path)
+        {
+            this.MyFolderPath = path;
+            this.MyFolderName = Path.GetFileName(path);
+            return this;
+        }
+
         public void WriteContentsToHTML(string name)
         {
-            name = DecideNameForHTML(name);
-
+            StepLevel++;
+            name = DecideFileNameForHTML(name);
 
             using (StreamWriter output_file = new StreamWriter(name, true))
             {
@@ -147,13 +144,12 @@ namespace CreateHTMLpicFolder
                 output_file.WriteLine("****************<br>Content Num = " + ContentList.Count.ToString() + "<br>****************<br>");
                 output_file.WriteLine("<table border = \"5\">");
 
-
                 foreach (string content in ContentList)
                 {
-                    WriteImg(output_file, content_index, content);
+                    DoWriteContentsToHTML(output_file, content_index, content);
                     content_index++;
                 }
-                if (content_index % MAX_COLUMN != 0)
+                if (content_index % ParaMaxColumn != 0)
                 {
                     output_file.WriteLine("\t</tr>");
                 }
@@ -165,68 +161,69 @@ namespace CreateHTMLpicFolder
             }
         }
 
-        const int MAX_COLUMN = 3;
-        private void WriteImg(StreamWriter output_file, int content_index, string content)
+        public void WriteFoldersLinkToHTML(string name_input)
         {
-            StringBuilder sb = new StringBuilder();
-
-            if (content_index % MAX_COLUMN == 0)
-            {
-                sb.Append("\t<tr>");
-                sb.Append("\n");
-            }
-            sb.Append("\t\t<td> <img src='");
-            var content_name_for_img = content;
-            if(content_name_for_img.Contains('\''))
-            {
-                content_name_for_img = content_name_for_img.Replace("'", "&#39;");
-                WriteToConsole(5, content + "->" + content_name_for_img);
-            }
-            sb.Append(content_name_for_img);
-            sb.Append(".jpg' height='384' width='364'></img> <br> <a href=\"");
-            sb.Append(content);
-            sb.Append("\">CLICK</a> </td>");
-
-            if (content_index % MAX_COLUMN == (MAX_COLUMN - 1))
-            {
-                sb.Append("\n\t</tr>");
-            }
-            output_file.WriteLine(sb.ToString());
-        }
-
-        private string DecideNameForHTML(string name)
-        {
-            if (name == null)
-            {
-                name = MyFolderPath + "\\" + MyFolderName + HTML;
-            }
-            else
-            {
-                name = MyFolderPath + "\\" + name + HTML;
-            }
-
-            return name;
-        }
-
-        public void WriteFoldersLinkToHTML(string name)
-        {
-            name = DecideNameForHTML(name);
-            using (StreamWriter output_file = new StreamWriter(name, false))
+            StepLevel++;
+            var filename = DecideFileNameForHTML(name_input);
+            using (StreamWriter output_file = new StreamWriter(filename, false))
             {
                 output_file.WriteLine("<head>\n<meta charset = \"UTF-8\"> </head>");
-
+                output_file.WriteLine("<body style = \"background-color:#" + ParaBackgroundColor + ";\" >\n</body>");
                 if (MyFolderLevel != 0)
                 {
                     output_file.WriteLine("<a href=\"../" + ParentFolderName + ".html\">back</a><br>");
                 }
                 foreach (FolderWithFileAndFolder folder in FolderList)
                 {
-                    WriteWithAHref(output_file, folder.GetFolderName());
+                    DoWriteFoldersLinkToHTML(output_file, folder.GetFolderName());
                 }
             }
         }
 
-        private void WriteWithAHref(StreamWriter output_file, string str)
+        private string DecideFileNameForHTML(string name_input)
+        {
+            var filename = "";
+            if (name_input == null)
+            {
+                filename = MyFolderPath + "\\" + MyFolderName + STR_HTML;
+            }
+            else
+            {
+                filename = MyFolderPath + "\\" + name_input + STR_HTML;
+            }
+
+            return filename;
+        }
+
+        private void DoWriteContentsToHTML(StreamWriter output_file, int content_index, string content)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (content_index % ParaMaxColumn == 0)
+            {
+                sb.Append("\t<tr>");
+                sb.Append("\n");
+            }
+            sb.Append("\t\t<td> <img src='");
+            var content_name_for_img = content;
+            if (content_name_for_img.Contains('\''))
+            {
+                content_name_for_img = content_name_for_img.Replace("'", "&#39;");
+                ShowToConsole(StepLevel, content + "->" + content_name_for_img);
+            }
+            sb.Append(content_name_for_img);
+            sb.Append(".jpg' height='" + ParaPicHeight.ToString() + "' width='" + ParaPicWidth.ToString() + "'></img> <br> <a href=\"");
+            sb.Append(content);
+            sb.Append("\">CLICK</a> </td>");
+
+            if (content_index % ParaMaxColumn == (ParaMaxColumn - 1))
+            {
+                sb.Append("\n\t</tr>");
+            }
+            output_file.WriteLine(sb.ToString());
+        }
+
+        private void DoWriteFoldersLinkToHTML(StreamWriter output_file, string str)
         {
             StringBuilder sb = new StringBuilder();
             sb.Append("<a href =\"");
@@ -239,14 +236,6 @@ namespace CreateHTMLpicFolder
             sb.Append("</a>");
             sb.Append("<br>");
             output_file.WriteLine(sb.ToString());
-        }
-
-
-
-        private void GetAllContentSelf()
-        {
-            GetFilesUnder();
-            GetContentsFromFileList();
         }
 
         private List<string> ExtractJPGList()
@@ -265,12 +254,26 @@ namespace CreateHTMLpicFolder
                         if (!jpg_list.Contains(filename8))
                         {
                             jpg_list.Add(filename8);
-                            WriteToConsole(2, "New JPG: " + filename8);
+                            ShowToConsole(StepLevel, "New JPG: " + filename8);
                         }
                     }
                 }
             }
             return jpg_list;
+        }
+        private void ShowToConsole(int step, string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int counti = 0; counti < MyFolderLevel; counti++)
+            {
+                sb.Append(STR_LEVEL_MARK);
+            }
+            for (int counti = 0; counti < step; counti++)
+            {
+                sb.Append(STR_STEP_MARK);
+            }
+
+            Console.WriteLine(sb.ToString() + str);
         }
     }
 }
--- CreateHTMLpicFolder/Program.cs
+++ CreateHTMLpicFolder/Program.cs
@@ -8,23 +8,117 @@ namespace CreateHTMLpicFolder
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            var arg_len = args.Length;
+            bool dostuff;
+            dostuff = ExtractArgPara(args, arg_len);
+            if (dostuff)
+            {
+                DoStuff();
+            }
+        }
+
+        private static bool ExtractArgPara(string[] args, int arg_len)
+        {
+            var dostuff = true;
+            for (int arg_index = 0; arg_index < arg_len; ++arg_index)
+            {
+                string arg = args[arg_index];
+                if (arg == "-h" || arg == "-?" || arg == "/?" || arg == "/h")
+                {
+                    dostuff = ShowHelpToConsole();
+                    break;
+                }
+                else if (arg_index + 1 < arg_len)
+                {
+                    var arg_para = args[arg_index + 1];
+                    SetParameter(arg, arg_para);
+                }
+                else
+                {                   
+                    //do nothing
+                }
+            }
+
+            return dostuff;
+        }
+
+        private static void SetParameter(string arg, string arg_para)
+        {
+            if (arg == "-r")
+            {
+                FolderWithFileAndFolder.ParaBackgroundColor = arg_para;
+            }
+            else
+            {
+                try
+                {
+                    var para = int.Parse(arg_para);
+                    if (para > 1)
+                    {
+                        switch (arg)
+                        {
+                            case "-col":
+                                FolderWithFileAndFolder.ParaMaxColumn = para;
+                                break;
+                            case "-w":
+                                FolderWithFileAndFolder.ParaPicWidth = para;
+                                break;
+                            case "-v":
+                                FolderWithFileAndFolder.ParaPicHeight = para;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                    //                Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private static bool ShowHelpToConsole()
+        {
+            bool dostuff;
+            WriteAppInfoToConsole();
+            Console.WriteLine("-col NN : Max Column Num");
+            Console.WriteLine("-w NNN: Pic Width");
+            Console.WriteLine("-v NNN : Pic Height");
+            Console.WriteLine("-r RRGGBB : Background color");
+
+            dostuff = false;
+            return dostuff;
+        }
+
+        private static void DoStuff()
+        {
             string root_path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            FolderWithFileAndFolder root_folder = FolderWithFileAndFolder.GetNewFolder().SetFolderPath(root_path);
-            Console.WriteLine("Content with .jpg searcher v0.1b");
-            Console.WriteLine("                              by syuu 2020");
-            Console.WriteLine("ROOTPATH: "+root_folder.GetFolderPath());
-            Console.WriteLine("ROOTNAME: "+root_folder.GetFolderName());
+            FolderWithFileAndFolder root_folder = FolderWithFileAndFolder.GetNewInstance().SetFolderPath(root_path);
+            WriteAppInfoToConsole();
+            Console.WriteLine("ROOTPATH: " + root_folder.GetFolderPath());
+            Console.WriteLine("ROOTNAME: " + root_folder.GetFolderName());
             //root_folder.GetFoldersUnder();
             //root_folder.GetFilesUnder();
             //root_folder.GetContentsFromFileList();
             //root_folder.WriteFoldersLinkToHTML(null);
             //root_folder.WriteContentsToHTML(null);
-            root_folder.SearchAllContentUnder(0, null);
-            Console.WriteLine("\nContent with .jpg searcher v0.1b");
-            Console.WriteLine("                              by syuu 2020");
+            root_folder.SearchAll(0, null);
+            WriteAppInfoToConsole();
+            Console.WriteLine("..... Press Any Key to exit.");
             Console.ReadLine();
+        }
+
+        private static void WriteAppInfoToConsole()
+        {
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("\nContent with .jpg preview Searcher v1.0.200624.1");
+            Console.WriteLine("                              by syuu 2020\n");
+            Console.WriteLine("---------------------------------------------------------");
         }
     }
 }
